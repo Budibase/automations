151b01923928874a89eb073c04ed9c0547338ea1
mike12345567 <me@michaeldrury.co.uk>
2020-10-13 13:54:31 +0100

    Adding a platform to zap integration so that it will work even if user doesn't supply any values.

--- packages/zapier/src/index.js
+++ packages/zapier/src/index.js
@@ -3,7 +3,10 @@ const axios = require("axios")
 module.exports = async function ({ inputs }) {
   const { url, value1, value2, value3, value4, value5 } = inputs
 
+  // send the platform to make sure zaps always work, even
+  // if no values supplied
   const result = await axios.post(url, {
+    platform: "budibase",
     value1,
     value2,
     value3,
